b537ef3c549fabc850d50f8b96bfadefe2fe9d91
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-03-09 11:32:26 +0100

    Update starterkit for v1.70.0-rc.4

--- src/imgly/config/features.ts
+++ src/imgly/config/features.ts
@@ -38,6 +38,9 @@ export function setupFeatures(cesdk: CreativeEditorSDK): void {
     // 'ly.img.duplicate', /* Duplicate button and copy/paste */
     // 'ly.img.group', /* Group and Ungroup buttons */
     // 'ly.img.replace', /* Replace button in Canvas Menu and Fill Panel */
+    // 'ly.img.replace.fill', /* Replace image/video fill content */
+    // 'ly.img.replace.shape', /* Replace block shape */
+    // 'ly.img.replace.audio', /* Replace audio block content */
     // 'ly.img.options', /* Options button (...) in Inspector Bar */
 
     /* ============================== */
